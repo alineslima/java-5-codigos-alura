c70c7ddeaf12c4a1fe843810aa8fa456860a7404
alineslima <alineslima@outlook.com>
2020-09-07 14:50:29 -0300

    Códigos Java 5 do curso da Alura Signed-off-by: alineslima <alineslima@outlook.com>

---
+++ java-5-codigo/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.bytebank.banco.modelo;
+
+/**
+* Classe que representa um cliente no Bytebank.
+*
+*@author Aline
+*@version 0.1
+*/
+public class Cliente { 
+    private String nome; 
+    private String cpf; 
+    private String profissao; 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() { 
+        return cpf;
+    }
+
+    public String getProfissao() { 
+        return profissao; 
+    }
+
+    public void setProfissao(String profissao) { 
+        this.profissao = profissao;
+    } 
+} 
